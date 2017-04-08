613335a6a102fdbc2a9c48baaea8ae92096902cf
Людмила <ludmila.burlaka@gmail.com>
2017-04-08 11:39:29 +0300

    update com.js

--- com.js
+++ com.js
@@ -1,9 +1,9 @@
-t1.onclick = function() {
-  document.getElementById('table1').classList.remove('hide');
-  document.getElementById('table2').classList.add('hide');
+f1.onclick = function() {
+  document.getElementById('form1').classList.remove('hide');
+  document.getElementById('form2').classList.add('hide');
 }
 
-t2.onclick = function() {
-  document.getElementById('table2').classList.remove('hide');
-  document.getElementById('table1').classList.add('hide');
+f2.onclick = function() {
+  document.getElementById('form2').classList.remove('hide');
+  document.getElementById('form1').classList.add('hide');
 }
